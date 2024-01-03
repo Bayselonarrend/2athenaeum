42c938e4382894a17eb86164fde54e2818b6bb1e
Anton Titovets <bayselonarrend@gmail.com>
2024-01-03 14:25:55 +0300

    Откат к ISAPI

--- nodejs/node_modules/winax/test/wscript/arguments.js
+++
@@ -1,8 +0,0 @@
-WScript.Echo("Same count: " + (WScript.Arguments.length==WScript.Arguments.Count()) );
-
-objArgs = WScript.Arguments
-WScript.Echo(WScript.Arguments.Count());
-for (i=0; i<objArgs.length; i++)
-{
-    WScript.Echo(objArgs(i))
-}
